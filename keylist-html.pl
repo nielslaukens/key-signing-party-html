11d3e11668da7ade4254ba90c1e642626aad142a
Niels Laukens <niels.laukens@vrt.be>
2015-02-12 12:00:55 +0100

    Collapse identical UIDs which only differ in email and/or comment

--- keylist-html.pl
+++ keylist-html.pl
@@ -6,6 +6,12 @@
 
 use strict;
 use warnings;
+use File::Temp qw/tempdir/;
+use HTML::Entities;
+use File::Basename;
+use Imager::QRCode;
+use MIME::Base64;
+
 
 my $keyring = "keyring.gpg";
 if( @ARGV == 1 ) {
@@ -22,7 +28,9 @@ binmode STDOUT, ':encoding(utf8)';
 
 my $tempdir = tempdir(CLEANUP => 1);
 my $rv = system("gpg", "--homedir", $tempdir, "-q", "--import", $keyring) >> 8;
-if( $rv != 0 ) { die "Could not import keyring"; }
+if( $rv != 0 ) {
+	die "Usage: $0 keyring.gpg\n";
+}
 
 open my $keys_fh, "gpg --homedir \"$tempdir\" --list-keys --with-fingerprint --with-colons --fixed-list-mode |"
         or die "Could not list keys";
@@ -42,6 +50,32 @@ sub format_fingerprint {
 	return join " ", @fp;
 }
 
+sub unique_uids {
+# This needs to reflect your policy!
+	my %uid;
+	my $uat = 0;
+	for my $uid (@_) {
+		if( $uid->{type} eq 'uid' ) {
+			my $cleanuid = $uid->{uid};
+			no warnings 'uninitialized';
+			$cleanuid =~ s/ <[^<]+>$//; # remove email-address
+			$cleanuid =~ s/ \([^(]+\)$//; # remove trailing comments
+			$cleanuid = encode_entities($cleanuid, "<>&");
+			$uid{ $cleanuid }++;
+		} elsif( $uid->{type} eq 'uat' ) {
+			$uat++
+		} else {
+			die "unknown UID type " . $uid->{type};
+		}
+	}
+	my @out = map { ($_ ne "" ? "<code>$_</code>" : "[undefined]") . " (" . $uid{$_} . ")" }
+		sort { length($b) <=> length($a) } keys(%uid);
+	if( $uat ) {
+		push @out, $uat . " UATs";
+	}
+	return @out;
+}
+
 
 print <<'EOT';
 	<html>
@@ -49,38 +83,62 @@ print <<'EOT';
 	  <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
 	  <title>FOSDEM keysigning event keylist</title>
 	  <style>
-	   @media print { pre {page-break-inside: avoid;} }
+		div.key {
+			border: 1pt solid black;
+			margin: 6pt;
+			padding: 4pt;
+		}
+		@media print { div.key {page-break-inside: avoid;} }
+		div.num {
+			font-size: 130%;
+			display: inline-block;
+			margin-right: 12pt;
+		}
+		div.pub {
+			display: inline-block;
+		}
+		div.fingerprint:before {
+			content: "";
+			border: 1pt solid black;
+			width: 10pt;
+			height: 10pt;
+			display: inline-block;
+			margin-right: 5pt;
+		}
+		div.uid:before {
+			content: "";
+			border: 1pt solid black;
+			width: 10pt;
+			height: 10pt;
+			display: inline-block;
+			margin-right: 5pt;
+		}
 	  </style>
 	 </head>
 	 <body>
-	  <pre>
 EOT
 
-my $i = 0;
+my $i = 1;
 for my $key (@key) {
-	printf "%03d  [ ] Fingerprint OK        [ ] ID OK\n", ++$i;
-	printf "pub   %s%s%d/%s %s%s\n",
+	print '<div class="key">';
+	print "<div class=\"num\">" . $i++ . "</div>";
+	printf "<div class=\"pub\">pub   %s%s%d/%s %s%s</div>\n",
 		$KeylistParseRaw::algo{ $key->{algo} },
 		(defined $key->{curve} ? $key->{curve} : ""),
 		$key->{key_length},
 		substr($key->{keyid},8), epoch_to_date($key->{create_date}),
 		($key->{expire_date} ne '' ? " [expires: " . epoch_to_date($key->{expire_date}) . "]" : "");
-	print "      Key fingerprint = ", format_fingerprint($key->{fingerprint}), "\n";
-	for my $uid (@{$key->{uid}}) {
-		if( $uid->{type} eq 'uid' ) {
-			if( defined $uid->{uid} ) {
-				printf "uid %s\n", encode_entities($uid->{uid}, "<>&");                    
-			} else {
-				print "<malformed UTF-8 uid>\n";
-			}
-		} elsif( $uid->{type} eq 'uat' ) {
-			#printf "uat %d packet(s), %d byte(s)\n", $uid->{packets}, $uid->{total_size};
-		} else {
-			die "Unknown uid type";
-		}
+	print "<div class=\"fingerprint\">Key fingerprint = ",
+		format_fingerprint($key->{fingerprint}),
+		"</div>\n";
+
+	# Try to reduce the number of UIDs
+	my @uid = unique_uids @{$key->{uid}};
+
+	for my $uid (@uid) {
+		print "<div class=\"uid\">$uid</div>\n";
 	}
-	print "---------------------------------------------------------------------</pre><pre>\n";
-	print "\n";
+	print "</div>\n";
 }
 
 print <<'EOT'
